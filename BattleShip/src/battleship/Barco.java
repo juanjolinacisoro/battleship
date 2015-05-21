40cc5fd5c8e8e881c2c0594845e4406861df9ed9
Revillot <drevillot10@alumnos.utalca.cl>
2015-05-20 23:59:03 -0400

    Tarea Final

--- BattleShip/src/battleship/Barco.java
+++ BattleShip/src/battleship/Barco.java
@@ -1,50 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package battleship;
 
 /**
  *
- * @author Usuario
+ * @author Javier gomez
  */
-public class Barco 
+public class Barco  /*esta clase asigna de manera aleatoria los barcos en el 
+tablero de ambos jugadores Se asigna una matriz de 10x10, despues a esta matriz 
+se rellena con 0, en este caso el 0 significa agua. Luego se colocan los barcos,
+el primer barco que se coloca es el de tamaÃ±o 5, luego el de tamaÃ±o 4, los 2  de
+tamaÃ±o 3 y finalmente el de tamaÃ±o 2. La manera de asignar los barcos es la 
+siguiente, primero se genera un nÃºmero aleatorio en donde si este nÃºmero es 1 el 
+barco se gana de manera horizontal  caso contrario se gana de manera vertical. 
+Luego se asigna una coordenada al azar de donde ira la primera posicion del 
+barco, en caso de ser vertical es desde esa posicion hacia abajo, en caso de ser
+horizontal es de esa posicion hasta la derecha. asignaro un cuadrante se 
+verifica que no este fuera del tablero y que no haya un barco en esas 
+posiciones, caso contrario se asigna  una nueva posicion. Esto ocurre para cada
+uno de los tamaÃ±os de los barcos
+    
+*/
 {
-        int[][] tablero;
-        String[][] tablero1;
+        int[][] tablero; //declaracion de un tablero
+        String[][] tablero1; //declaracion del segundo tablero
         
         public Barco()
         {
-            this.tablero = new int[10][10];
-            this.tablero1 = new String[10][10];
-            this.inicializarTablero();
+            this.tablero = new int[10][10];  //se dice que el tablero es de 10x10
+            this.tablero1 = new String[10][10]; //se dice el tablero 1 es de 10x10
+            this.inicializarTablero();  //se crea el metodo inicializar tablero
         }
         
         public void inicializarTablero()
         {
-            for(int in=0; in<10 ; in++)
+            for(int in=0; in<10 ; in++) //se recorre el tablero, el cual inicialmente va a tener puros 0, los cuales representan el agua
             {
                 for(int j=0; j<10; j++)
                 {
                     tablero[in][j]=0;
                 }
             }
+            //primero se asigna el barco de tamaÃ±o 5
             int C; //variable aleatoria que me determina si el barco va vertical u horizontal
-            C =  (int) (2*Math.random());  //0 vertical  y 1 horizontal //0 vertical  y 1 horizontal //0 vertical  y 1 horizontal //0 vertical  y 1 horizontal //0 vertical  y 1 horizontal //0 vertical  y 1 horizontal //0 vertical  y 1 horizontal //0 vertical  y 1 horizontal
-            if(C==1)
+            C =  (int) (2*Math.random());  //0 vertical  y 1 horizontal 
+            if(C==1)   //en caso de que sea horizontal
             {
                 int i;
-                i=(int) (9 * Math.random());
+                i=(int) (9 * Math.random()); //se asigna una coordenada i al azar
                 int j;
-                j= (int) (9 * Math.random());
-                while(j >=5 || tablero[i][j]!=0 || tablero[i][j+1]!=0 || tablero[i][j+2]!=0 || tablero[i][j+3]!=0 || tablero[i][j+4]!=0)
+                j= (int) (9 * Math.random()); //se asigna una coordenada j al azar
+                while(j >=5 || tablero[i][j]!=0 || tablero[i][j+1]!=0 || tablero[i][j+2]!=0 || tablero[i][j+3]!=0 || tablero[i][j+4]!=0) //si j es mayor a 5 el barco no cabe en el tablero y si las piezas que vienen mas abajo no son agua no se puede colocar el barco
                 {
-                   j= (int) (9 * Math.random()); 
-                   i=(int) (9 * Math.random());
+                   j= (int) (9 * Math.random());  //se asigna una nueva coordenada en caso de que haya un barco ya asignado o el barco no caiga en el tablero
+                   i=(int) (9 * Math.random()); //se asigna una nueva coordenada
 
                 }
-                tablero[i][j]=1;
+                //en caso de que las coordenadas donde esta el barco no se salgan de margen y que en esas coordenadas a no hay un barco instalado se procese a actualizar la matriz.
+                //el 1 representa que existe en barco
+                tablero[i][j]=1; 
                 tablero[i][j+1]=1;
                 tablero[i][j+2]=1;
                 tablero[i][j+3]=1;
@@ -52,6 +64,7 @@ public class Barco
             }
             else
             {
+                //este caso representa el mismo anterior pero con los barcos colocados de manera vertical
                 int i;
                 i=(int) (9 * Math.random());
                 int j;
@@ -68,8 +81,8 @@ public class Barco
                 tablero[i+3][j]=1;
                 tablero[i+4][j]=1;
             }
-            // barco de tamaÃ±o 4
-            C = (int) (2*Math.random()); //0 vertical  y 1 horizontal//0 vertical  y 1 horizontal//0 vertical  y 1 horizontal//0 vertical  y 1 horizontal//0 vertical  y 1 horizontal//0 vertical  y 1 horizontal//0 vertical  y 1 horizontal//0 vertical  y 1 horizontal
+            // barco de tamaÃƒÂ±o 4
+            C = (int) (2*Math.random()); //0 vertical  y 1 horizontal
             if(C==1)
             {
                 int i;
@@ -103,7 +116,7 @@ public class Barco
                 tablero[i+2][j]=1;
                 tablero[i+3][j]=1;
             }
-            //barco de tamaÃ±o 3 
+            //barco de tamaÃƒÂ±o 3 
             for(int n=0; n<2 ; n++)
             {
                  C = (int) (2*Math.random()); //0 vertical  y 1 horizontal
@@ -138,7 +151,7 @@ public class Barco
                 tablero[i+2][j]=1;
                 }
             }
-            //barco tamaÃ±o 2
+            //barco tamaÃƒÂ±o 2
             C = (int) (2*Math.random()); //0 vertical  y 1 horizontal//0 vertical  y 1 horizontal//0 vertical  y 1 horizontal//0 vertical  y 1 horizontal//0 vertical  y 1 horizontal//0 vertical  y 1 horizontal//0 vertical  y 1 horizontal//0 vertical  y 1 horizontal
                 if(C==1)
                 {
--- BattleShip/src/battleship/Tablero.java
+++ BattleShip/src/battleship/Tablero.java
@@ -1,17 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package battleship;
 
 /**
  *
- * @author Usuario
+ * @author Juanjo_Linacisoro <jlinacisoro10@alumnos.utalca.cl>
  */
 public class Tablero 
 {
-    
+    /*
+    Los atributos de esta clase Tablero son disparojugadorx(PosiciÃ³n de la coordenada x del disparo, disparojugadory (PosiciÃ³n de la coordenada y del disparo), tablero1 (ActualizaciÃ³n del tablero)
+    */
     public int disparojugadorx;
     public int disparojugadory;
     public String[][] tablero1;
@@ -19,6 +17,9 @@ public class Tablero
     
     public Tablero () //debiera de pedir el tablero actual y el disparo
     {
+        /*
+        InicializaciÃ³n de los atributos de esta clase.
+        */
         this.disparojugadorx =0; // se supone que va el lugar validado
         this.disparojugadory =0; // se supone que va el tiro validado
         this.tablero1= new String[10][10];
@@ -26,6 +27,12 @@ public class Tablero
     
     public String[][] actualizarTablero(int x, int y, String[][] tablero)
     {
+    /*
+    Metodo de la clase Tablero actualiza la matriz dependiendo se se a acertado a algÃºn disparo con una "H" o en caso contrario se bombardeo al agua (representado con una "X").
+    @Param el metodo recibe la posicion x e y  del tablero que se desea validar
+    @Return verdadero si se alcanza la suma de puntos para ganar, o falso si aÃºn no se llega a ese puntaje.
+    @Version Final
+    */
         this.disparojugadorx = x; // se supone que va el lugar validado
         this.disparojugadory = y; // se supone que va el tiro validado
         this.tablero1=tablero;
