c72620375453e2c6208a64eb43ea593108c127a1
flavioencina <fencina10@alumnos.utalca.cl>
2015-05-27 11:56:27 -0300

    comentarios

--- BattleShip/src/battleship/Tablero.java
+++ BattleShip/src/battleship/Tablero.java
@@ -27,11 +27,11 @@ public class Tablero
     
     public String[][] actualizarTablero(int x, int y, String[][] tablero)
     {
-    /*
-    Metodo de la clase Tablero actualiza la matriz dependiendo se se a acertado a algÃºn disparo con una "H" o en caso contrario se bombardeo al agua (representado con una "X").
-    @Param el metodo recibe la posicion x e y  del tablero que se desea validar
-    @Return verdadero si se alcanza la suma de puntos para ganar, o falso si aÃºn no se llega a ese puntaje.
-    @Version Final
+    /**
+    *Metodo de la clase Tablero actualiza la matriz dependiendo se se a acertado a algÃºn disparo con una "H" o en caso contrario se bombardeo al agua (representado con una "X").
+    *@Param el metodo recibe la posicion x e y  del tablero que se desea validar
+    *@Return verdadero si se alcanza la suma de puntos para ganar, o falso si aÃºn no se llega a ese puntaje.
+    *@Version Final
     */
         this.disparojugadorx = x; // se supone que va el lugar validado
         this.disparojugadory = y; // se supone que va el tiro validado
